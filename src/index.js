b5ead6e8da1402b84141011f468b054e1bca37dd
Kyle Leonard <kdleonard@icloud.com>
2019-09-03 22:50:54 -0500

    Installed dependencies and imported react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+<Router>
+  ReactDOM.render(
+  <App />, document.getElementById("root"));
+</Router>;
